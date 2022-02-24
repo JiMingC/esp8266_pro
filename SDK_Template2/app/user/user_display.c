373eb54ede64e00137a85f6145fc1e1bb1e0216a
jiming <623516556@qq.com>
2022-02-24 10:37:22 +0800

    <type> :add new time display mode[2]
    <iusse>:add new display mode
    <solve>:add
    <date> :02-24

--- SDK_Template2/app/user/user_display.c
+++ SDK_Template2/app/user/user_display.c
@@ -10,9 +10,11 @@
 #define LCD_H_HALF	LCD_H/2
 #define LCD_W_HALF	LCD_W/2
 
+#define TimeDisplayT	2
+
 void ICACHE_FLASH_ATTR
 DisplayTime(u8 sntpTupdate,SntpData_t sntpdata) {
-#if 0
+#if TimeDisplayT == 0
 	if (sntpTupdate > 1) {
 		MLCD_ShowChar(LCD_W_HALF-32+16, LCD_H_HALF, ':', 2);
 		MLCD_ShowChar(LCD_W_HALF-32+40, LCD_H_HALF, ':', 2);
@@ -26,7 +28,7 @@ DisplayTime(u8 sntpTupdate,SntpData_t sntpdata) {
 	if (sntpTupdate & 0x1)
 		MLCD_Show2Num(LCD_W_HALF-32, LCD_H_HALF, sntpdata.hour, 2);
 	os_printf("%d, %d, %d\n",sntpdata.hour, sntpdata.minute, sntpdata.second );
-#else
+#elif TimeDisplayT == 1
 	if (sntpTupdate > 1) {
 		MLCD_ShowChar(0+34, LCD_H_HALF, ':', 4);
 		MLCD_ShowChar(0+76, LCD_H_HALF, ':', 4);
@@ -39,6 +41,20 @@ DisplayTime(u8 sntpTupdate,SntpData_t sntpdata) {
 	sntpTupdate >>= 1;
 	if (sntpTupdate & 0x1)
 		MLCD_Show2Num(0+4, LCD_H_HALF, sntpdata.hour, 4);
+	os_printf("%d, %d, %d\n",sntpdata.hour, sntpdata.minute, sntpdata.second );
+#elif TimeDisplayT == 2
+	if (sntpTupdate > 1) {
+		MLCD_ShowChar(40, LCD_H_HALF, ':', 4);
+		MLCD_ShowChar(86, LCD_H_HALF + 16, ':', 2);
+	}
+	if (sntpTupdate & 0x1)
+		MLCD_Show2Num(94, LCD_H_HALF + 16, sntpdata.second, 2);
+	sntpTupdate >>= 1;
+	if (sntpTupdate & 0x1)
+		MLCD_Show2Num(54, LCD_H_HALF, sntpdata.minute, 4);
+	sntpTupdate >>= 1;
+	if (sntpTupdate & 0x1)
+		MLCD_Show2Num(10, LCD_H_HALF, sntpdata.hour, 4);
 	os_printf("%d, %d, %d\n",sntpdata.hour, sntpdata.minute, sntpdata.second );
 #endif
 }
